67942572d5973e0ffc799884ef1056fa9dec0d18
Keti-stosempreingito <122582919+stosempreingiro@users.noreply.github.com>
2023-11-24 22:43:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 sudo apt update
-echo "INSTALLING MACCHANGER"
+echo "INSTALLING MACCHANGER, GIT and Python3-Netifaces" && sleep 3
 sudo apt install macchanger -y
 sudo apt install git -y
 sudo apt install python3
 sudo apt install python3-pip -y
 python3 -m pip install netifaces
 sudo apt install python3-netifaces -y
-git clone https://github.com/stosempreingiro/MAChange.git
-cd MAChange
 sudo mv mac.py /usr/local/bin/mac
 sudo chmod +x /usr/local/bin/mac
 clear -x
