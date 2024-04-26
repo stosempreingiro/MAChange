94d651d566bf87e440d8414a341a291094342a3f
stosempreingiro <122582919+stosempreingiro@users.noreply.github.com>
2024-04-26 19:13:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 sudo apt update
-echo "INSTALLING MACCHANGER, GIT and Python3-Netifaces" && sleep 3
-sudo apt install macchanger -y
-sudo apt install python3 -y
-sudo apt install python3-pip -y
-python3 -m pip install netifaces
-sudo apt install python3-netifaces -y
+echo "INSTALLING MACCHANGER, GIT and Python3-Netifaces" && sleep 1
+sudo apt install macchanger python3 python3-pip -y && sudo apt install python3-netifaces || pip3 install netifaces
 sudo mv mac.py /usr/local/bin/mac
 sudo chmod +x /usr/local/bin/mac
 clear -x
@@ -13,6 +9,4 @@ echo "INSTALLAZIONE COMPLETATA"
 echo "[IT] Scrivi 'sudo mac' nel tuo terminale per cominciare"
 echo "[EN] Installatione complete. Write 'sudo mac' on your terminal."
 sleep 0.7
-echo " "
 echo "Source code: https://github.com/stosempreingiro/MAChange"
-echo "-stosempreingiro"
